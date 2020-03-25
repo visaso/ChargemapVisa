a97fb350d52b1491dd6095c87ada56cfd4135282
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2020-03-25 09:54:42 +0200

    first

--- models/station.js
+++ models/station.js
@@ -5,23 +5,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const stationSchema = new Schema({
-  Connections: Array, // TODO: add relation to Connections: https://mongoosejs.com/docs/populate.html
-  Title: String,
-  AddressLine1: String,
-  Town: String,
-  StateOrProvince: String,
-  Postcode: String,
-  Location: {
-    type: {
-      type: String, // Don't do `{ location: { type: String } }`
-      enum: ['Point'], // 'location.type' must be 'Point'
-      required: true,
-    },
-    coordinates: {
-      type: [Number], // longitude first
-      required: true,
-    },
-  },
+  //TODO: schema
 });
 
 module.exports = mongoose.model('Station', stationSchema);
