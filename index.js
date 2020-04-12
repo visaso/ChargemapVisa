de9605dbb967342f5f2a8f751d6acf0198dd76ce
visaso <32449867+visaso@users.noreply.github.com>
2020-04-12 17:04:13 +0300

    env

--- index.js
+++ index.js
@@ -98,19 +98,15 @@ io.on('connection', function(socket){
 });
 
 if (process.env.NODE_ENV === 'development') {
-
-} else {
-  
-}
-http.createServer((req, res) => {
-      res.writeHead(301, { 'Location': 'https://wisardster.jelastic.metropolia.fi' + req.url });
-      res.end();
+  http.createServer((req, res) => {
+    res.writeHead(301, { 'Location': 'https://localhost:8000' + req.url });
+    res.end();
 }).listen(3000);
-
-
+} else {
+  res.redirect('https://' + req.headers.host + req.url);
+}
 
 });
-
 
 
 
